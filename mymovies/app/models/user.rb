3c2d58a74c8ae7744c06add077f6504acc947d05
hamoghamdi <haneen.aljabrah@gmail.com>
2019-06-19 17:19:24 +0300

    pre-final, - readme.

--- mymovies/app/models/user.rb
+++ mymovies/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :user_movies
+  has_many :user_movies, dependent: :destroy
   has_many :movies, :through => :user_movies
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
